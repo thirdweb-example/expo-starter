9644f32012b3cb105aa767bad13065788a760ba0
Joaquim Verges <joaquim.verges@gmail.com>
2025-08-13 20:54:55 +1200

    release builds (#36)

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import { Platform } from "react-native";
+import "react-native-get-random-values";
+
+// Add polyfills for Node.js modules
+if (typeof global.Buffer === 'undefined') {
+  global.Buffer = require('buffer').Buffer;
+}
+
+if (typeof global.process === 'undefined') {
+  global.process = require('process');
+}
 
 if (Platform.OS !== "web") {
-	import("@thirdweb-dev/react-native-adapter");
+  import("@thirdweb-dev/react-native-adapter");
 }
 import "react-native-reanimated";
 import "expo-router/entry";
